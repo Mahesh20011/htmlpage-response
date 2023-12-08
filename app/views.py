fdedec378b5a252aca862ec55cff2a2b61a9cbec
Mahesh20011 <kondamahesh2001@gmail.com>
2023-12-08 17:16:34 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def mahesh(request):
+    return render(request,'mahesh.html')
+def hari(request):
+    return render(request,'hari.html')
+
